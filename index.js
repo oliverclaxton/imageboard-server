537090162bf4a9935e9f200630fba1e67e1f28fd
oliverclaxton <oliver.claxton66@gmail.com>
2020-10-28 17:03:29 +0100

    added packages and initialized sequelize-cli

--- index.js
+++ index.js
@@ -1,9 +1,7 @@
 const express = require("express");
 const app = express();
 const port = 4000;
-const cors = require("cors");
 
-app.use(cors());
 app.use(express.json());
 
 app.get("/", (req, res) => {
